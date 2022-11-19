bd06a0fdec1979cf9b54c40da7e5ef074392f84c
Jan Krokos <jankrokos@gmail.com>
2022-11-20 00:38:58 +0100

    Added bottom navigation bar and top app bar

--- app/src/main/java/com/example/bambino/MainActivity.kt
+++ app/src/main/java/com/example/bambino/MainActivity.kt
@@ -3,12 +3,39 @@ package com.example.bambino
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.bambino.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         // Handle the splash screen transition.
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.page_3 -> {
+                    // Respond to navigation item 3 click
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
